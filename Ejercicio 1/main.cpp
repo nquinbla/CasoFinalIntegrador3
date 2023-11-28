6d5b2e2daa94c8c34e31249993f71f1943b6178b
nquinbla <nquinbla@myuax.com>
2023-11-28 11:55:53 +0100

    commit prueba

---
+++ Ejercicio 1/main.cpp
@@ -0,0 +1,24 @@
+//1. Definir e implementar una clase Variant. Esta clase deberá ser capaz de representar diferentes tipos de datos, incluyendo símbolos, números, listas y procedimientos.
+//2. Implementar un método to_string() para la clase Variant que convierta una instancia de la clase a una cadena de texto.
+//3. Implementar un método to_json_string() para la clase Variant que convierta una instancia de la clase a una representación en formato JSON.
+//4. Implementar un método estático from_json_string() que tome una cadena en formato JSON y la convierta a una instancia de la clase Variant.
+//5. Implementar un método parse_json() que tome una cadena en formato JSON y la convierta a una instancia de la clase Variant.
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <initializer_list>
+#include <sstream>
+
+
+using namespace std;
+
+class Variant {
+
+
+
+
+
+};
+
