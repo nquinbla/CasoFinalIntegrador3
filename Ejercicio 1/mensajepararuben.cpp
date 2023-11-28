d729167b71ff1d5eb0ce53fe3e0d0567249bf114
nquinbla <nquinbla@myuax.com>
2023-11-28 12:25:26 +0100

    FINAL, un 0

---
+++ Ejercicio 1/mensajepararuben.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    cout << "Hola profe, viva extremoduro, viva la tarta de queso, viva la oreja de van gogh, muerte a json, todos odiamos a json, no quiero saber nada de json, danos una pista para el examen ruben" << endl;
+    return 0;
+}
