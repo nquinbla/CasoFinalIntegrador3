00a8b568c53d7e7678798b1bd1d1da6965606117
nquinbla <nquinbla@myuax.com>
2023-11-28 11:58:56 +0100

    prueba 2

--- Ejercicio 1/prueba1.cpp
+++ Ejercicio 1/prueba1.cpp
@@ -1,3 +1,29 @@
-//
-// Created by nerea on 28/11/2023.
-//
+//Definir e implementar una clase Variant. Esta clase deberá ser capaz de representar diferentes tipos de datos, incluyendo símbolos, números, listas y procedimientos.
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <initializer_list>
+#include <sstream>
+#include <functional>
+
+using namespace std;
+
+class Variant {
+public:
+    enum class Type { SYMBOL, NUMBER, LIST, PROCEDURE
+    };
+    Variant() : type(Type::SYMBOL), symbol("") {}
+    Variant(string symbol) : type(Type::SYMBOL), symbol(symbol) {}
+    Variant(double number) : type(Type::NUMBER), number(number) {}
+    Variant(vector<Variant> list) : type(Type::LIST), list(list) {}
+    Variant(function<Variant(vector<Variant>)> procedure) : type(Type::PROCEDURE), procedure(procedure) {}
+    Type type;
+    string symbol;
+    double number;
+    vector<Variant> list;
+    function<Variant(vector<Variant>)> procedure;
+};
+
+
